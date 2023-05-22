ed40783a0514533e9f3544a480371a02e4fc3e0e
bingsanyu <66949416+bingsanyu@users.noreply.github.com>
2023-05-23 00:54:15 +0800

    代码重构

--- feishumiaoji.py
+++ feishumiaoji.py
@@ -1,51 +1,28 @@
 # -*- coding:utf-8 -*-
-import codecs
-import os.path
+import os
 import threading
-import time
 from contextlib import closing
-import urllib.parse
+import time
 import requests
-import pandas as pd
 from tqdm import tqdm
 
+
 class MultiDownloader:
-    def __init__(self, url, save_path=None, file_name=None, thread_count=10, headers=None):
+    def __init__(self, headers, url, file_name, thread_count=20):
+        self.headers = headers
         self.url = url
-        self.headers = headers if isinstance(headers, dict) else {}
-        current_file_path = os.path.dirname(os.path.abspath(__file__))
-        self.save_path = save_path or os.path.join(current_file_path, "multi_download")
-        self.total_range = None
-        self.get_resp_header_info()
         self.file_name = file_name
-        self.file_lock = threading.Lock()
         self.thread_count = thread_count
-        self.failed_thread_list = []
+        self.chunk_size = 1024 * 1024
+        self.total_range = self.get_file_size()
         self.finished_thread_count = 0
-        self.chunk_size = 1024 * 100
+        self.file_lock = threading.Lock()
 
-    def get_logger(self):
-        logger = logging.getLogger("MultiDownloader")
-        logger.setLevel(logging.INFO)
-        formatter = logging.Formatter("%(asctime)s-%(filename)s-line:%(lineno)d-%(levelname)s-%(process)s: %(message)s")
-        console_handler = logging.StreamHandler()
-        console_handler.setLevel(logging.INFO)
-        console_handler.setFormatter(formatter)
-        if not os.path.exists(self.save_path):
-            os.mkdir(self.save_path)
-        file_handler = logging.FileHandler(os.path.join(self.save_path, "download.log"), encoding="utf-8")
-        file_handler.setLevel(logging.INFO)
-        file_handler.setFormatter(formatter)
-        logger.addHandler(console_handler)
-        logger.addHandler(file_handler)
-        return logger
-
-    def get_resp_header_info(self):
-        res = requests.head(self.url, headers=self.headers, allow_redirects=True)
-        res_header = res.headers
-        content_range = res_header.get("Content-Length", "0")
-        self.total_range = int(content_range)
-        self.url = res.url
+    def get_file_size(self):
+        res = requests.head(self.url, headers=self.headers)
+        if res.status_code == 200:
+            return int(res.headers.get('Content-Length'))
+        return None
 
     def page_dispatcher(self, content_size):
         page_size = content_size // self.thread_count
@@ -66,7 +43,7 @@ class MultiDownloader:
         range_headers |= self.headers
         try_times = 3
         for _ in range(try_times):
-            with closing(requests.get(url=self.url, headers=range_headers, stream=True, timeout=30)) as res:
+            with requests.get(url=self.url, headers=range_headers, stream=True, timeout=30) as res:
                 if res.status_code == 206:
                     for data in res.iter_content(chunk_size=self.chunk_size):
                         with self.file_lock:
@@ -75,7 +52,6 @@ class MultiDownloader:
                         page["start_pos"] += len(data)
                     break
         self.finished_thread_count += 1
-
 
     def run(self):
         if not self.total_range or self.total_range < 1024:
@@ -91,35 +67,29 @@ class MultiDownloader:
                 thread.join()
         if os.path.exists(self.file_name) and os.path.getsize(self.file_name) == 0:
             os.remove(self.file_name)
-if __name__ == '__main__':
-    cookie = 改成你的cookie
-    bv_csrf_token = 改成你的bv_csrf_token
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
-        'cookie': cookie,
-        'referer': 'https://se6llxwh0q.feishu.cn/minutes/me',
-        'content-type': 'application/x-www-form-urlencoded',
-        'bv-csrf-token': bv_csrf_token,
-    }
 
-    # # 会议信息
-    # df = pd.read_csv('飞书妙记1.csv')
-    # existing_meeting_ids = df['meeting_id'].tolist()
-    # existing_meeting_ids=[]
-    # 遍历会议
-    get_rec_url = 'https://se6llxwh0q.feishu.cn/minutes/api/space/list?&size=1000&space_name=2'
-    resp = requests.get(url=get_rec_url, headers=headers)
-    print(resp.json())
-    df_to_append = pd.DataFrame()
-    all_meetings = list(reversed(resp.json()['data']['list']))
-    for index in tqdm(all_meetings, desc='Processing Meetings', unit=' meeting'):
+class MeetingDownloader:
+    def __init__(self, cookie, bv_csrf_token):
+        self.cookie = cookie
+        self.bv_csrf_token = bv_csrf_token
+        self.headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
+            'cookie': self.cookie,
+            'referer': 'https://se6llxwh0q.feishu.cn/minutes/me',
+            'content-type': 'application/x-www-form-urlencoded',
+            'bv-csrf-token': self.bv_csrf_token,
+        }
+
+    def get_meeting_info(self):
+        get_rec_url = 'https://se6llxwh0q.feishu.cn/minutes/api/space/list?&size=1000&space_name=2'
+        resp = requests.get(url=get_rec_url, headers=self.headers)
+        return list(reversed(resp.json()['data']['list']))
+
+    def download_meeting_video(self, index):
         meeting_id = index['meeting_id']
-#         if meeting_id in existing_meeting_ids:
-#             print(f"Meeting with meeting_id {meeting_id} already exists in the CSV file. Skipping...")
-#             continue
         object_token = index['object_token']
         get_download_url = f'https://se6llxwh0q.feishu.cn/minutes/api/status?object_token={object_token}&language=zh_cn&_t={int(time.time() * 1000)}'
-        resp = requests.get(url=get_download_url, headers=headers)
+        resp = requests.get(url=get_download_url, headers=self.headers)
         download_url = resp.json()['data']['video_info']['video_download_url']
         start_time = time.strftime("%Y年%m月%d日%H时%M分", time.localtime(index['start_time'] / 1000))
         stop_time = time.strftime("%Y年%m月%d日%H时%M分", time.localtime(index['stop_time'] / 1000))
@@ -133,25 +103,36 @@ if __name__ == '__main__':
             'video_cover': index['video_cover'],
         }
         file_name = f'{start_time}至{stop_time}' + index['topic'].replace('|', '').replace(' ', '')
-        run_params = {'headers': headers,
+        run_params = {'headers': self.headers,
                       'url': download_url,
                       "file_name": f'{file_name}/{file_name}.mp4',
                       "thread_count": 20
                       }
         downloader = MultiDownloader(**run_params)
         downloader.run()
-        # 下载妙记SRT文件
+        return dit, file_name
+
+    def download_subtitle(self, object_token, file_name):
         params = {'add_speaker': 'true',
                   'add_timestamp': 'true',
                   'format': '3',
                   'object_token': object_token,
                   }
         srt_url = 'https://se6llxwh0q.feishu.cn/minutes/api/export'
-        print(dit)
-
-        # pd.DataFrame([dit]).to_csv("飞书妙记1.csv", 'a')
-        resp = requests.post(url=srt_url, params=params, headers=headers)
+        resp = requests.post(url=srt_url, params=params, headers=self.headers)
         resp.encoding = "utf-8"
         with open(f"{file_name}/{file_name}.txt", "w+") as f:
-           f.write(resp.text)
-        
+            f.write(resp.text)
+
+    def download_all_meetings(self):
+        all_meetings = self.get_meeting_info()
+        for index in tqdm(all_meetings, desc='Processing Meetings', unit=' meeting'):
+            dit, file_name = self.download_meeting_video(index)
+            self.download_subtitle(index['object_token'], file_name)
+            print(dit)
+
+if __name__ == '__main__':
+    cookie = input('cookie: ')
+    bv_csrf_token = input('bv_csrf_token: ')
+    downloader = MeetingDownloader(cookie, bv_csrf_token)
+    downloader.download_all_meetings()
